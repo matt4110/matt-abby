2062dfd759b1b2ed2a36b88f970d419e1394c753
matt4110 <matthew.johnson4110@gmail.com>
2016-06-27 22:17:47 -0400

    start from scratch, baseline structure and styles

---
+++ footer.php
@@ -0,0 +1,4 @@
+	<script   src="https://code.jquery.com/jquery-3.0.0.min.js"   integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="   crossorigin="anonymous"></script>
+	<script type="text/javascript" src="/assets/js/script.js"></script>
+</body>
+</html>
