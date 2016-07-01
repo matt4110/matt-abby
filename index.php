1e1376d4e1a5f9f1a4bd6bec09f295df8168af33
matt4110 <matthew.johnson4110@gmail.com>
2016-07-01 14:39:36 -0400

    built content section of homepage

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php include 'header.php'; ?>
-<div class="content">
+	<div class="content">
+
+	<div style="margin-bottom:-4px;" class="grid-1-1">
+		<img src="/photos/terp_wed_01.jpg" alt="Terpstra bride and groom photo">
+	</div>
+
+	<div class="row">
+		<div class="grid-4-12">
+			<img src="/photos/terp_wed_02.jpg" alt="Terpstra brdie and groom photo">
+		</div>
+		<div class="grid-4-12">
+			<img src="/photos/terp_wed_03.jpg" alt="wedding flowers photo">
+		</div>
+		<div class="grid-4-12">
+			<img src="/photos/terp_wed_04.jpg" alt="bride and groom photo on bench">
+		</div>
+	</div>
+
+	<button class="button"><a href="portfolio">see our<br>work</a></button>
 	
 </div>
 <?php include 'footer.php'; ?>
